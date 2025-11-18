4ecaf00eb4e3533f91f7ebd790eaa3e7c3c4037d
shaquile6789 <shaquille.oemrawsingh26@compscihigh.org>
2025-11-18 20:03:27 +0000

    finished api.js

--- api.js
+++ api.js
@@ -5,3 +5,12 @@ const apiKey = process.env.apiKey2
 // run your code with node api.js
 // add your apiKey to .env if your API needs one, otherwise ignore
 // Follow the checklist on the Performance Assessment Google Doc
+let pUrl = "https://www.thesportsdb.com/api/v1/json/123/searchplayers.php?p="
+
+async function player(pUrl){
+    const response = await fetch(pUrl)
+    const data = await response.json(pUrl + prompt("Choose a player: "))
+    console.log(data)
+}
+
+player(pUrl)
